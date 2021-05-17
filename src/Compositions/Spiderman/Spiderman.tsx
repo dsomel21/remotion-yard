a9730df0316462f15d4b92058296a23e64b099ee
Dilraj Somel <dsomel21@gmail.com>
2021-05-17 16:44:08 -0400

    Added styles for Spiderman/VidyardExample comp

--- src/Compositions/Spiderman/Spiderman.tsx
+++ src/Compositions/Spiderman/Spiderman.tsx
@@ -4,13 +4,28 @@ import Captions from '../Captions';
 import spidermanVideo from '../../assets/spidermanVideo.mp4';
 import spidermanText from '../../assets/spidermanTranscribed.json';
 
+const spidermanStyles = {
+  fontFamily: 'Arial',
+  fontStyle: 'italic',
+  color: '#ffeaa7',
+  fontWeight: 800,
+  fontSize: '63px',
+  textShadow:
+    '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000',
+  textTransform: 'lowercase',
+};
+
 const Spiderman: React.FC = () => {
   const { fps } = useVideoConfig();
 
   return (
     <>
       <Video src={spidermanVideo} />
-      <Captions transcribedObj={spidermanText} fps={fps} />
+      <Captions
+        transcribedObj={spidermanText}
+        fps={fps}
+        customStyles={spidermanStyles}
+      />
     </>
   );
 };
