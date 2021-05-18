aed914e825d113118f981eb7d009d59590d0b17f
Dilraj Somel <dsomel21@gmail.com>
2021-05-17 21:04:17 -0400

    Minor code clean up. Need to write up a README

--- src/services/render.js
+++ src/services/render.js
@@ -6,13 +6,13 @@ const argv = yargs(hideBin(process.argv)).argv;
 (function render() {
   console.log(`🕵🏽‍♀️ Searching for component: ${argv.comp}`);
   try {
-    if (!argv.comp) {
+    if (typeof argv.comp !== 'string') {
       throw new Error(
         'Invalid ID. Enter the component ID that you want to render'
       );
     }
     const compName = argv.comp;
-    console.log('💃🏾 Starting render!');
+    console.log(`💃🏾 Attempting to render ${compName}`);
     exec(`cd ../../`);
     const renderCommand = exec(
       `npx remotion render src/index.tsx ${compName} tmp/${Date.now()}_${compName}.mp4`
